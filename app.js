837a45b30ea99f9645ddf8100a56ae2a6b65dd09
António Carlos Alves de Sousa <antonio_carlos_alves_sousa@hotmail.com>
2018-04-25 23:31:43 +0100

    commit 25 april

--- app.js
+++ app.js
@@ -22,8 +22,12 @@ const config = require('./config.json');
 /**
  * Host Configuration
  */
+// como esta aplicação está o "deployed" na cloud temos está configuração obrigatoria para o PORT
+// process.env.PORT -> configuração necessária para o heroku executar a API
+// config.app_port -> para execução local (DEV)
 const PORT = process.env.PORT || config.app_port || 8080;
-const SERVER_ROOOT = config.app_url + ":" + PORT;
+// ligação necessária apenas nos clientes
+// const SERVER_ROOT = config.app_url + ":" + PORT;
 
 /**
  * Database Configuration (MongoDB on ATLAS)
@@ -31,6 +35,7 @@ const SERVER_ROOOT = config.app_url + ":" + PORT;
 mongoose.connect(config.db_host[0] + config.db_user + ":" + config.db_pass
     + config.db_host[1] + config.db_name + config.db_host[2]);
 
+// uso do Mongoose Promise, para usarmos algumas callbacks em algumas das querys
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, "MongoDB connection error: "));
@@ -38,10 +43,17 @@ db.on('error', console.error.bind(console, "MongoDB connection error: "));
 /**
  * Express Object Initialize
  */
+// framerwork / library express usada para termos a possiblidade
+// de usar Handlers para os diferentes pedidos verbos HTTP
+// (e.g GET, POST, DELETE, PUT, PATCH, ...)
 var app = express();
+// uso do bodyParser para tratarmos os dados provenientes do req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // set the view engine to ejs
+// ejs é um engine de visualização que nos permite facilmente transportar
+// dados do backend para o front end, carregando dados dinamicos através
+// do js para as nossas paginas HTML. <%= %>
 app.set('view engine', 'ejs');
 
 /**
--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,24 @@ const express = require('express')
  * @Return documentation about API
  * @Method [GET]
  */
-router.get('/', function(req, res){
-    res.status(200).render('index');
+router.get('/', function (req, res) {
+    // res.status(200).render('index');
+    // exemplo para passagem de dados (argumentos, objetos, etc) para
+    // o frontend atráves do EJS
+    let infoArray = {
+        owners: [
+            {
+                name: "António Carlos Alves de Sousa",
+                number: "1170300"
+            },
+            {
+                name: "Gonçalo Reais",
+                number: "1170303"
+            }
+        ],
+        version: "0.0.1"
+    }
+    res.status(200).render('index', { title: "Index Page", infoArray: infoArray });
 });
 
 
